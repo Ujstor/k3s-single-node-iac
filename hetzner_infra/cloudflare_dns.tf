f8a760959cbd59e0ed5ba3a2a065f07ebafe026c
Ujstor <astipan@gmail.com>
2024-11-24 23:48:10 +0100

    plausible-analytics

--- hetzner_infra/cloudflare_dns.tf
+++ hetzner_infra/cloudflare_dns.tf
@@ -66,6 +66,14 @@ module "cloudflare_record" {
       ttl     = 60
       proxied = false
     }
+    analytics = {
+      zone_id = var.cloudflare_zone_id
+      name    = "plausible-analytics"
+      content = module.k3s_server.server_info.k3s-server.ip
+      type    = "A"
+      ttl     = 60
+      proxied = false
+    }
   }
   depends_on = [module.k3s_server]
 }
